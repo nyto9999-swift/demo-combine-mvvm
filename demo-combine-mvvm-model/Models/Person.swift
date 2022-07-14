1a57a130072f00376ce3bf274096dcda39792097
ninjachen <nyto9999@gmail.com>
2022-07-14 18:57:42 +0800

    git init

---
+++ demo-combine-mvvm-model/Models/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  demo-combine-mvvm
+//
+//  Created by 宇宣 Chen on 2022/7/14.
+//
+
+import Foundation
+
+class Person {
+  var name:String
+  
+  init(name: String) {
+    self.name = name
+  }
+}
